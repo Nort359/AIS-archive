da0451953d5d6903dffeb5ea28e0923f894ff7bb
Maxim Prosvirkin <Nort359@gmail.com>
2017-05-01 15:01:54 +0300

    Finished settings of project! Final

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,42 @@
 /**
  * Created by Nort3 on 01.05.2017.
  */
-const webpack = require('webpack');
-const path = require('path');
-
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: './src/index.jsx',
+
     output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: '/public'
     },
-    plugins: [
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify('production')
-        }),
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings:       false,
-                drop_console:   true,
-                unsafe:         true
-            }
-        })
-    ],
-    resolve: {
-        modulesDirectories: ['node_modules', 'bower_components'],
-        moduleTemplates: ['*', 'index'],
-        extensions: ['', '.js'],
-        root: __dirname + '/src'
+
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3000
     },
-    resolveLoader: {
-        modulesDirectories: ['node_modules', 'bower_components'],
-        moduleTemplates: ['*-loader', '*'],
-        extensions: ['', '.js']
-    },
+
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['react-hot', 'babel-loader'],
-                include: [
-                    path.resolve(__dirname, 'src')
-                ],
-                plugins: ['transform-runtime']
+                loader: 'react-hot-loader!babel-loader'
             },
             {
-                test: /\.less$/,
-                loader: 'style' +
-                '!css?sourceMap' +
-                '!autoprefixer-loader?browsers=last 2 version' +
-                '!less?sourceMap=source-map-less-inline'
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                loader: 'file?name=img/[path][name].[ext]'
-            },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file?name=fonts/[name].[ext]'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml&name=fonts/[name].[ext]'
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
+    },
+
+    devtool: 'eval-source-map',
+
+    resolve: {
+        extensions: ['.js', '.jsx']
     }
 };
